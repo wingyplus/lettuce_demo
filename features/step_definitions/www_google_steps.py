872266938c8e551acc537063e4c4003e877345d0
Thanabodee <thanabodee@osdev.co.th>
2013-10-13 23:06:10 +0700

    change expect result to string and refactor steps

--- features/step_definitions/www_google_steps.py
+++ features/step_definitions/www_google_steps.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-from lettuce import step, world
+from lettuce import after, before, step, world
 from splinter.browser import Browser
+
+
+@before.all
+def initialize_browser():
+    world.browser = Browser("firefox")
+
+
+@after.all
+def quit_browser(total_result):
+    world.browser.quit()
 
 
 @step(u'Given I am on Google Search')
 def given_i_am_on_google_search(step):
-    world.browser = Browser()
     world.browser.visit("http://www.google.co.th")
 
 
@@ -15,8 +24,7 @@ def when_i_search_keyword(step, keyword):
     world.browser.find_by_id("gbqfb").click()
 
 
-@step(u'Then I see title of first result should begin with Cucumber')
-def then_i_see_title_of_result_should_begin_with_cucumber(step):
+@step(u'Then I see title of first result should begin with "([^"]*)"')
+def then_i_see_title_of_result_should_begin_with_keyword(step, keyword):
     result = world.browser.find_by_css("#rso a > em")
-    assert result.text.startswith("Cucumber") is True
-    world.browser.quit()
+    assert result.text.startswith(keyword) is True
